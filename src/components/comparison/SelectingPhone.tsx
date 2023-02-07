5cd75ec97aeb3b2d32335e1bd150ff201e1b6da1
js43o <js43o@naver.com>
2023-02-07 20:43:38 +0900

    feat: 비교 페이지의 선택 기기 해제 버튼 추가

--- src/components/comparison/SelectingPhone.tsx
+++ src/components/comparison/SelectingPhone.tsx
@@ -1,9 +1,10 @@
 import styled from '@emotion/styled';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import { Box, IconButton, Typography } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import { Phone } from 'utils/types';
-import { searchingModeState } from 'utils/atoms';
+import { comparisonDevicesState, searchingModeState } from 'utils/atoms';
 
 const PhoneImage = styled.img`
   width: 100%;
@@ -15,6 +16,9 @@ const PhoneImage = styled.img`
 export default function SelectingPhone(props: { phone?: Phone; slot: 1 | 2 }) {
   const { phone, slot } = props;
   const setSearchingMode = useSetRecoilState(searchingModeState);
+  const [comparisonDevice, setComparisonDevice] = useRecoilState(
+    comparisonDevicesState
+  );
 
   const onOpenSearcing = () => {
     setSearchingMode({
@@ -23,11 +27,20 @@ export default function SelectingPhone(props: { phone?: Phone; slot: 1 | 2 }) {
     });
   };
 
+  const unselectDevice = () => {
+    if (slot === 1) {
+      setComparisonDevice([undefined, comparisonDevice[1]]);
+      return;
+    }
+    setComparisonDevice([comparisonDevice[0], undefined]);
+  };
+
   return (
     <Box
       sx={{
         display: 'flex',
         flexDirection: 'column',
+        alignItems: 'center',
       }}
     >
       {phone ? (
@@ -37,6 +50,9 @@ export default function SelectingPhone(props: { phone?: Phone; slot: 1 | 2 }) {
             {phone.name}
           </Typography>
           <Typography variant="subtitle1">{phone.korName}</Typography>
+          <IconButton onClick={unselectDevice}>
+            <RemoveCircleOutlineIcon fontSize="large" color="error" />
+          </IconButton>
         </>
       ) : (
         <Box
