4842d05314584e19493730e1b7f9f2315440349f
js43o <js43o@naver.com>
2023-02-09 20:46:59 +0900

    style: 크기 비교 섹션 스타일 변경

--- src/components/comparison/SizeComparison.tsx
+++ src/components/comparison/SizeComparison.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 import {
   Box,
   Typography,
-  Grid,
   FormGroup,
   FormControlLabel,
   Switch,
+  Divider,
   useMediaQuery,
   useTheme,
 } from '@mui/material';
@@ -15,6 +15,7 @@ import { Phone } from 'utils/types';
 
 const ImageWrapper = styled(Box)<{ layered: boolean; minWidth?: number }>`
   display: flex;
+  flex-flow: wrap;
   align-items: flex-end;
   position: relative;
   min-width: ${({ layered, minWidth }) => layered && minWidth};
@@ -69,12 +70,18 @@ export default function SizeComparison(props: {
       <Box
         sx={{
           display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
+          flexDirection: 'column',
+          gap: 1,
         }}
       >
-        <Typography variant="h2">크기 비교</Typography>
-        <FormGroup>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="h2">크기 비교</Typography>
           <FormControlLabel
             control={
               <Switch
@@ -86,6 +93,37 @@ export default function SizeComparison(props: {
             label="레이어 뷰"
             labelPlacement="start"
           />
+        </Box>
+        <Divider />
+        <FormGroup>
+          {device1 && (
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={visible1}
+                  value={visible1}
+                  onChange={(e) => onVisibleChange(e, 1)}
+                  color="secondary"
+                />
+              }
+              label={device1.name}
+              labelPlacement="start"
+            />
+          )}
+          {device2 && (
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={visible2}
+                  value={visible2}
+                  onChange={(e) => onVisibleChange(e, 2)}
+                  color="secondary"
+                />
+              }
+              label={device2.name}
+              labelPlacement="start"
+            />
+          )}
         </FormGroup>
       </Box>
       <Box
@@ -93,105 +131,68 @@ export default function SizeComparison(props: {
           display: 'flex',
           flexFlow: 'wrap',
           gap: 2,
+          minHeight:
+            Math.max(
+              device1?.design.demension[0] || 100,
+              device2?.design.demension[0] || 100
+            ) * offset,
         }}
       >
-        <Box
-          sx={{
-            display: 'flex',
-            flexFlow: 'wrap',
-            gap: 2,
-            minHeight:
-              Math.max(
-                device1?.design.demension[0] || 100,
-                device2?.design.demension[0] || 100
-              ) * offset,
-          }}
+        {!device1 && !device2 && (
+          <Typography variant="body1">선택된 기기가 없습니다.</Typography>
+        )}
+        <ImageWrapper
+          layered={layered}
+          minWidth={
+            Math.max(
+              device1?.design.demension[1] || 0,
+              device2?.design.demension[1] || 0
+            ) * offset
+          }
         >
-          <ImageWrapper
-            layered={layered}
-            minWidth={
-              Math.max(
-                device1?.design.demension[1] || 0,
-                device2?.design.demension[1] || 0
-              ) * offset
-            }
-          >
-            {device1 && visible1 && (
-              <Image
-                src={`/images/size/${device1.url}-front.webp`}
-                alt={device1.url}
-                height={device1.design.demension[0] * offset}
-                width={device1.design.demension[1] * offset}
-              />
-            )}
-            {device2 && visible2 && (
-              <Image
-                src={`/images/size/${device2.url}-front.webp`}
-                alt={device2.url}
-                height={device2.design.demension[0] * offset}
-                width={device2.design.demension[1] * offset}
-              />
-            )}
-          </ImageWrapper>
-          <ImageWrapper
-            layered={layered}
-            minWidth={
-              Math.max(
-                device1?.design.demension[2] || 0,
-                device2?.design.demension[2] || 0
-              ) * offset
-            }
-          >
-            {device1 && visible1 && (
-              <Image
-                src={`/images/size/${device1.url}-side.webp`}
-                alt={device1.url}
-                height={device1.design.demension[0] * offset}
-                width={device1.design.demension[2] * offset}
-              />
-            )}
-            {device2 && visible2 && (
-              <Image
-                src={`/images/size/${device2.url}-side.webp`}
-                alt={device2.url}
-                height={device2.design.demension[0] * offset}
-                width={device2.design.demension[2] * offset}
-              />
-            )}
-          </ImageWrapper>
-        </Box>
-        <Box>
-          <FormGroup
-            sx={{
-              flexDirection: 'column',
-            }}
-          >
-            {device1 && (
-              <FormControlLabel
-                control={
-                  <Switch
-                    checked={visible1}
-                    value={visible1}
-                    onChange={(e) => onVisibleChange(e, 1)}
-                  />
-                }
-                label={device1.name}
-              />
-            )}
-            {device2 && (
-              <FormControlLabel
-                control={
-                  <Switch
-                    checked={visible2}
-                    value={visible2}
-                    onChange={(e) => onVisibleChange(e, 2)}
-                  />
-                }
-                label={device2.name}
-              />
-            )}
-          </FormGroup>
-        </Box>
+          {device1 && visible1 && (
+            <Image
+              src={`/images/size/${device1.url}-front.webp`}
+              alt={device1.url}
+              height={device1.design.demension[0] * offset}
+              width={device1.design.demension[1] * offset}
+            />
+          )}
+          {device2 && visible2 && (
+            <Image
+              src={`/images/size/${device2.url}-front.webp`}
+              alt={device2.url}
+              height={device2.design.demension[0] * offset}
+              width={device2.design.demension[1] * offset}
+            />
+          )}
+        </ImageWrapper>
+        <ImageWrapper
+          layered={layered}
+          minWidth={
+            Math.max(
+              device1?.design.demension[2] || 0,
+              device2?.design.demension[2] || 0
+            ) * offset
+          }
+        >
+          {device1 && visible1 && (
+            <Image
+              src={`/images/size/${device1.url}-side.webp`}
+              alt={device1.url}
+              height={device1.design.demension[0] * offset}
+              width={device1.design.demension[2] * offset}
+            />
+          )}
+          {device2 && visible2 && (
+            <Image
+              src={`/images/size/${device2.url}-side.webp`}
+              alt={device2.url}
+              height={device2.design.demension[0] * offset}
+              width={device2.design.demension[2] * offset}
+            />
+          )}
+        </ImageWrapper>
       </Box>
     </Box>
   );
