d39f9dbd4bc0e3ff25cdf69571a615ca5c3d0285
js43o <js43o@naver.com>
2023-02-11 14:00:48 +0900

    feat: 크기 비교 요약 텍스트 추가

--- src/components/comparison/SizeComparison.tsx
+++ src/components/comparison/SizeComparison.tsx
@@ -53,8 +53,6 @@ export default function SizeComparison(props: {
     if (slot === 2) setVisible2(event.target.checked);
   };
 
-  const toggleLayered = () => setLayered(!layered);
-
   return (
     <Box
       sx={{
@@ -87,7 +85,7 @@ export default function SizeComparison(props: {
               <Switch
                 value={layered}
                 checked={layered}
-                onChange={toggleLayered}
+                onChange={() => setLayered(!layered)}
               />
             }
             label="레이어 뷰"
@@ -194,6 +192,45 @@ export default function SizeComparison(props: {
           )}
         </ImageWrapper>
       </Box>
+      <Divider />
+      {device1 && device2 && (
+        <Box sx={{}}>
+          <Typography variant="body1">
+            {device1.name}에 비해 <b>{device2.name}</b>이 세로로{' '}
+            <b>
+              {Math.abs(
+                device2.design.demension[1] - device1.design.demension[1]
+              ).toFixed(2)}
+              mm
+            </b>
+            {device2.design.demension[1] > device1.design.demension[1]
+              ? ' 더 크며, '
+              : ' 더 작으며, '}
+            가로로{' '}
+            <b>
+              {Math.abs(
+                device2.design.demension[0] - device1.design.demension[0]
+              ).toFixed(2)}
+              mm
+            </b>
+            {device2.design.demension[0] > device1.design.demension[0]
+              ? ' 더 큽니다.'
+              : ' 더 작습니다.'}
+          </Typography>
+          <Typography variant="body1">
+            두께는{' '}
+            <b>
+              {Math.abs(
+                device2.design.demension[2] - device1.design.demension[2]
+              ).toFixed(2)}
+              mm
+            </b>
+            {device2.design.demension[2] > device1.design.demension[2]
+              ? ' 더 두껍습니다.'
+              : ' 더 얇습니다.'}
+          </Typography>
+        </Box>
+      )}
     </Box>
   );
 }
